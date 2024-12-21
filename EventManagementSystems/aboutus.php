a4e25a259972f72e5829203e234a19616bac924b
sushmita077 <sushmitanagekar@gmail.com>
2024-12-21 15:07:24 +0530

    commited

---
+++ EventManagementSystems/aboutus.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>College Event Management</title>
+<?php require 'utils/styles.php'; ?><!--css links. file found in utils folder-->
+</head>
+<style>
+
+/* Large rounded green border */
+hr.blueline {
+  border: 10px solid #00004d;
+  border-radius: 5px;
+}
+
+#bj
+{
+  font-size: 22px;
+}
+</style>
+
+<body style="background-color:#b3e0ff ">
+
+  <?php require 'utils/header.php'; ?>
+  <hr class="blueline">
+  <div class="col-md-12">
+  
+<h1>About Us</h1>
+
+<p style="font-style:italic" id="bj"> To be a premier institution in Technology and Management by fostering excellence in education, innovation, incubation and values to inspire and empower the young minds</p>
+<p style="font-style:italic" id="bj"> PSahyadri College of Engineering and Management, Mangalore was established in 2007 under the Bhandary Foundation and is an autonomous institute affiliated with Visvesvaraya Technological University (VTU), Belagavi. SCEM Mangalore offers B.E /B.Tech, MBA and Ph.D programmes.
+The collaborations of Sahyadri College of Engineering and Management, Mangalore are Technical Career Education Pvt Ltd (TCE), Karnataka Biotechnology & Information Technology Services, Mangalore University, Federation of Karnataka Chambers of Commerce & Industries, Reliance Jio Infocom Limited and many more.
+<p style="font-style:italic" id="bj">  Synargia is a cultural fest which is held every year, we like everyone to participate cheerfully in every event.</p>
+
+</div>
+<hr class="blueline">
+</body>
+
+ <?php require 'utils/footer.php'; ?>
+
+</html>
---
+++ EventManagementSystems/utils/footer.php
@@ -0,0 +1,38 @@
+<hr class = "footerline"><!--css modified horizontal line-->
+<footer>
+    <div class = "container">
+        <div class = "row">
+            <section>
+                <div class = "footerContent col-md-4"><!--left content-->
+                    <p class = "footerContent1">
+                        <strong>S</strong><span class = "small footerSubtext">ayhadri</span>
+                        <!-- <strong>D</strong><span class = "small footerSubtext">ri</span> -->
+                        <strong>C</strong><span class = "small footerSubtext">ollege of </span>
+                        <strong>EN</strong><span class = "small footerSubtext">eering and management</span>
+                    </p>
+
+                    <p class = "footerSubtext2">
+                    Our college Mission is to impart quality technical education and higher moral ethics associated with skilled training to suit the modern day technology with innovative concepts,so as to learn to lead the future with full confidence
+                  .
+                    </p>
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--middle content-->
+                <p> <br>
+                 Address: Sahyadri college of Engineering and management
+                    <br>Adyar Manglore 575007.</p>
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--right content-->
+                    Follow Us:<br>
+                    <a href="https://www.facebook.com/@Sahyadri.edu/"><img src = "images/facebook.png"></a>
+                    <a href="https://www.instagram.com/sahyadri_college?igsh=MTkxY3ZpOWExOTQzdg=="><img src = "images/instagram.png" width="65px"></a>
+                        
+                        <a href="https://youtu.be/eIj2UsrUsFw?si=6cdfNvsGYlJOnGCk"><img src = "images/youtube.png"></a>
+                </div>
+            </section>
+        </div>
+    </div>
+</footer>
